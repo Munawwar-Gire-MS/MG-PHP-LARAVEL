75c3d3d25f09aa91b7925733d4b23c08f8cfd9bc
munawwar-gire-ms <v-mgire@microsoft.com>
2024-09-04 06:41:50 +0000

    NEw Commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return '<h1 style="color: red;">Hello This is Munawwar\'s</h1> <h1 style="color: blue;"> Laravel Hello Project</h1>';
+});
+
